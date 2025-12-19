455f82fc950c9e282364728392217987f06f4cd0
saniya-dogra <saniyadogra2@gmail.com>
2025-12-19 20:50:13 +0530

    model

---
+++ ml/model.py
@@ -0,0 +1,13 @@
+# model.py
+
+import joblib
+from config import MODEL_PATH
+
+class ApprovalModel:
+    def __init__(self):
+        self.model = joblib.load(MODEL_PATH)
+
+    def predict_probability(self, monthly_income, cibil, loan_amount):
+        X = [[monthly_income, cibil, loan_amount]]
+        probability = self.model.predict_proba(X)[0][1]
+        return round(probability, 4)
