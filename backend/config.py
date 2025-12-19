169e93f0688aca5a2147c7f2831fcc548bf1415c
saniya-dogra <saniyadogra2@gmail.com>
2025-12-19 20:48:34 +0530

    config

---
+++ backend/config.py
@@ -0,0 +1,20 @@
+# config.py
+
+# Application settings
+DEBUG = True
+PORT = 5000
+
+# ML model path
+MODEL_PATH = "ml/model.pkl"
+
+# Supported banks (future expansion)
+BANKS = [
+    "Bank of Maharashtra"
+]
+
+# RBI / Bank constants
+MAX_LOAN_TENURE_YEARS = 30
+MAX_AGE_AT_MATURITY = 75
+
+# Currency
+CURRENCY = "INR"
