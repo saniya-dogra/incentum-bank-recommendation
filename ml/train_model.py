f62567063e04711a232d6cbcd9521bb239410258
saniya-dogra <saniyadogra2@gmail.com>
2025-12-19 20:36:48 +0530

    train_model

---
+++ ml/train_model.py
@@ -0,0 +1,10 @@
+from sklearn.ensemble import RandomForestClassifier
+import pandas as pd, joblib
+
+df = pd.read_csv("train.csv")
+X = df[["income","cibil","loan"]]
+y = df["approved"]
+
+model = RandomForestClassifier(n_estimators=300)
+model.fit(X,y)
+joblib.dump(model,"model.pkl")
