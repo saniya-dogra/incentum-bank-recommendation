e1fea10cdde01715d522e850df15807318d1b74d
saniya-dogra <saniyadogra2@gmail.com>
2025-12-20 18:27:43 +0530

    import files

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
+
+
 from flask import Flask, request, jsonify
 from rules.eligibility_engine import bom_engine
 from utils.validators import validate_input
