de4462ff32feddc5190a8483efa0e16d09113a9b
saniya-dogra <saniyadogra2@gmail.com>
2026-08-29 00:50:04 +0530

    Add changes

--- ml/train_model.py
+++ ml/train_model.py
@@ -1,31 +1,3 @@
-# # ml/train_model.py
-# import pandas as pd
-# import joblib
-# from sklearn.linear_model import LogisticRegression
-# from sklearn.preprocessing import StandardScaler
-# from sklearn.pipeline import Pipeline
-
-# df = pd.read_csv("ml/train.csv")
-
-# X = df[["income", "cibil", "loan"]]
-# y = df["approved"]
-
-# model = Pipeline([
-#     ("scaler", StandardScaler()),
-#     ("clf", LogisticRegression(
-#         max_iter=2000,
-#         class_weight="balanced"
-#     ))
-# ])
-
-# model.fit(X, y)
-# joblib.dump(model, "ml/model.pkl")
-
-# print("✅ Logistic Regression model trained")
-
-
-
-
 import pandas as pd
 import joblib
 
